58b4510dd567eac3e1f6b830f445b16f3ca83d26
Lucky Pratap <157149399+luckypratap23@users.noreply.github.com>
2025-09-22 14:58:03 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+   server: {
+    proxy: {
+      '/path': {
+        target: 'ai-lecture-summarizer-backend-production.up.railway.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  
 })
